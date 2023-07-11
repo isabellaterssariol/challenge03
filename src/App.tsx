013ed07c437396d23af892ab9904066dcfec7b7a
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-11 12:28:14 -0300

    Creating routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Root from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+         index: true,
+         element: <HomePage />,
+      },
+      {
+        // path: "/products",
+        // element: <AllProducts />,
+      },
+      {
+        // path: "/product",
+        // children: [
+         // {
+          //  path: ":productId",
+          //  element: <ProductPage />,
+          // },
+        //],
+      },
+      {
+        // path: "/shopping",
+        // element: <ShoppingCart />,
+      },
+    ],
+  },
+  {
+    // path: "*",
+    // element: <ErrorPage />,
+  }
+]);
 
 function App() {
-  return (
-    <>
-      <p>Hello World!</p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
