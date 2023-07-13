63be59cb017d719e185d077e28419188760ad24a
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-13 19:23:20 -0300

    Updating SearchPage.

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,12 @@
 import Search from "../components/Search"
+import Header from "../components/Header";
 
 const SearchPage = () => {
     return (
         <>
+            <Header showText={true}/>
             <Search />
+            <p>Popular product</p>
         </>
     )
 }
