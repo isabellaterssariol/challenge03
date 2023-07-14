d29d8825ceba95d02329b3248f9948af07775045
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-13 22:08:22 -0300

    Updating style to SearchPage and SearchCard.

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,17 +1,18 @@
 import Search from "../components/Search"
 import Header from "../components/Header";
 import SearchCard from "../components/SearchCard";
+import classes from "./SearchPage.module.css";
 
 const SearchPage = () => {
     return (
-        <>
+        <div className={classes.container}>
             <Header showText={true}/>
             <Search />
-            <p>Popular product</p>
+            <p className={classes.text}>Popular product</p>
             <SearchCard productId={1} />
             <SearchCard productId={5} />
             <SearchCard productId={22} />
-        </>
+        </div>
     )
 }
 
