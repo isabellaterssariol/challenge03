d6b16e913565a842a32e7829d8208ebd586e17e6
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-14 21:00:38 -0300

    Adding the library Splide in SomeProducts.

--- src/components/SomeProducts.tsx
+++ src/components/SomeProducts.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import ProductCard from "./ProductCard";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 interface SomeProductsProps {
     title: string;
@@ -13,7 +15,41 @@ const SomeProducts: React.FC<SomeProductsProps> = ({ title }) => {
                 <Link to="/">See all</Link>
                 </p>
             </div>
-            <ProductCard productId={4}/>
+            <div>
+                <Splide
+                    options={{
+                    perPage: 2,
+                    perMove: 1,
+                    arrows: false,
+                    pagination: false,
+                    }}>
+                    <SplideSlide>
+                        <ProductCard productId={4}/> 
+                    </SplideSlide>
+                    <SplideSlide>
+                        <ProductCard productId={6}/> 
+                    </SplideSlide>
+                    <SplideSlide>
+                        <ProductCard productId={22}/> 
+                    </SplideSlide>
+                    <SplideSlide>
+                        <ProductCard productId={31}/> 
+                    </SplideSlide>
+                    <SplideSlide>
+                        <ProductCard productId={22}/> 
+                    </SplideSlide>
+                    <SplideSlide>
+                        <ProductCard productId={25}/> 
+                    </SplideSlide>
+                    <SplideSlide>
+                        <ProductCard productId={15}/> 
+                    </SplideSlide>
+                    <SplideSlide>
+                        <ProductCard productId={27}/> 
+                    </SplideSlide>
+                </Splide>
+            </div>
+            
         </>
     )
 }
