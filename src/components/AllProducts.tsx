3d3957c6abee9c4cc07cf6b02d058800b24c69c6
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-15 16:05:16 -0300

    Updating logic to implement filter.

--- src/components/AllProducts.tsx
+++ src/components/AllProducts.tsx
@@ -1,21 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
 import classes from "./AllProducts.module.css"
 
-interface Product {
+interface ProductType {
   id: number;
+  name: string;
+  price: string;
+  rating: number;
+  category: string;
+  created_at: string;
+  reviews: ReviewType[];
+}
+
+interface ReviewType {
+  user: string;
+  description: string;
+  rating: number;
+  date: string;
+  id: number;
+}
+
+interface AllProductsProps {
+  filter: string;
+  category: string;
 }
 
 const apiUrl = "https://run.mocky.io/v3/8658d4c7-1a28-49a1-bfa8-801a536ba6c3";
 
-const AllProducts = () => {
-    const [products, setProducts] = useState<Product[]>([]);
+const AllProducts: React.FC<AllProductsProps> = ({ filter, category }) => {
+    const [products, setProducts] = useState<ProductType[]>([]);
   
     useEffect(() => {
         const findProducts = async () => {
             try {
-                const response = await axios.get<Product[]>(apiUrl);
+                const response = await axios.get<ProductType[]>(apiUrl);
                 setProducts(response.data);
             } catch (error) {
                 console.error("Error in finding products:", error);
@@ -23,11 +42,74 @@ const AllProducts = () => {
         };
         findProducts();
     }, []);
+
+    const filteredProducts = useMemo(() => {
+      let filtered = [...products];
+
+      if (category) {
+        filtered = filtered.filter((product) => product.category === category);
+      }
+
+      switch (filter) {
+        case "popularity":
+          filtered.sort((a, b) => 
+            b.rating - a.rating
+          );
+          break;
+
+        case "newest":
+          filtered.sort(
+            (a, b) =>
+              new Date(b.created_at).getTime() -
+              new Date(a.created_at).getTime()
+          );
+          break;
+
+        case "oldest":
+          filtered.sort(
+            (a, b) =>
+              new Date(a.created_at).getTime() -
+              new Date(b.created_at).getTime()
+          );
+          break;
+
+        case "high-price":
+          filtered.sort(
+            (a, b) =>
+              parseFloat(b.price.replace("$", "")) -
+              parseFloat(a.price.replace("$", ""))
+          );
+          break;
+
+        case "low-price":
+          filtered.sort(
+            (a, b) =>
+              parseFloat(a.price.replace("$", "")) -
+              parseFloat(b.price.replace("$", ""))
+          );
+          break;
+        
+        case "review":
+          filtered.sort((a, b) => 
+            b.reviews.length - a.reviews.length
+          );
+          break;
+
+        default:
+          console.log("Invalid option")
+      }
+  
+      return filtered;
+    }, [products, filter, category]);
   
     return (
       <div className={classes.container}>
-        {products.map((product) => (
-          <ProductCard productId={product.id} showReviews={true}/>
+        {filteredProducts.map((product) => (
+          <ProductCard 
+            productId={product.id} 
+            showReviews={true}
+            key={product.id}
+          />
         ))}
       </div>
     );
