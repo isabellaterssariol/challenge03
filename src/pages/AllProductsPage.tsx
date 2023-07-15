038afa6a736d8ed6893f584021a0b111bb3950c6
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-15 19:26:11 -0300

    Adding logic to show filter like a bottom sheet in AllProductsPage.

--- src/pages/AllProductsPage.tsx
+++ src/pages/AllProductsPage.tsx
@@ -1,16 +1,55 @@
 import AllProducts from "../components/AllProducts";
 import Header from "../components/Header";
-import classes from "./AllProductsPage.module.css"
+import classes from "./AllProductsPage.module.css";
+import { useState } from "react";
+import Filter from "../components/Filter";
 
 const AllProductsPage = () => {
+    const [showFilter, setShowFilter] = useState(false);
+    const [selectedFilter, setSelectedFilter] = useState("popularity");
+    const [selectedCategory, setSelectedCategory] = useState("");
+
+    const handleCloseFilter = () => {
+        setShowFilter(false);
+    };
+
+    const handleOpenFilter = () => {
+        setShowFilter(true);
+    };
+
+    const handleFilterChange = (filter: string) => {
+        setSelectedFilter(filter);
+    };
+
+    const handleCategoryChange = (category: string) => {
+        setSelectedCategory(category);
+    };
+
+    const handleApplyFilter = () => {
+        handleCloseFilter();
+    };
+    
     return (
         <div className={classes.container}>
             <Header />
             <p className={classes.text}>Featured products</p>
             <h1 className={classes.title}>See all products</h1>
-            <AllProducts />
+            <Filter 
+                selectedFilter={selectedFilter}
+                selectedCategory={selectedCategory}
+                handleFilterClick={handleFilterChange}
+                handleCategoryClick={handleCategoryChange}
+                applyFilter={handleApplyFilter}
+                showFilter={showFilter}
+                openFilter={handleOpenFilter}
+                closeFilter={handleCloseFilter}
+            />
+            <AllProducts 
+                filter={selectedFilter}
+                category={selectedCategory}
+            />
         </div>
-    )
-}
+    );
+};
 
 export default AllProductsPage;
