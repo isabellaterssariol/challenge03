bedc65f6cfb2b1e956d53094ba89a0704f8a8b4f
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-19 10:42:16 -0300

    Updating details about css.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,82 +1,91 @@
-import classes from "./Search.module.css"
+import classes from "./Search.module.css";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import SearchCard from "./SearchCard";
+import ProductListCard from "./ProductListCard";
 
 interface ProductType {
-    id: number;
-    name: string;
-    price: string;
-    rating: number;
-    reviews: ReviewType[];
+  id: number;
+  name: string;
+  price: string;
+  rating: number;
+  reviews: ReviewType[];
 }
-  
+
 interface ReviewType {
-    user: string;
-    description: string;
-    rating: number;
-    date: string;
-    id: number;
+  user: string;
+  description: string;
+  rating: number;
+  date: string;
+  id: number;
 }
 
 const apiUrl = "https://run.mocky.io/v3/2e274c48-e3d7-4b21-820a-c5c3de9f53f5";
 
 const Search = () => {
-    const navigate = useNavigate();
-    const [searchProduct, setSearchProduct] = useState("");
-    const [searchResult, setSearchResult] = useState<ProductType[]>([]);
+  const navigate = useNavigate();
+  const [searchProduct, setSearchProduct] = useState("");
+  const [searchResult, setSearchResult] = useState<ProductType[]>([]);
 
-    const handleInputClick = () => {
-        navigate("/search-page");
+  const handleInputClick = () => {
+    navigate("/search-page");
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchProduct(e.target.value);
+  };
+
+  useEffect(() => {
+    const findResult = async () => {
+      try {
+        const response = await axios.get<ProductType[]>(apiUrl);
+        const results = response.data.filter((item) =>
+          item.name.toLowerCase().includes(searchProduct.toLowerCase())
+        );
+        setSearchResult(results);
+      } catch (error) {
+        console.error("Error to search result:", error);
+      }
     };
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchProduct(e.target.value);
+    if (searchProduct.trim() !== "") {
+      findResult();
+    } else {
+      setSearchResult([]);
     }
+  }, [searchProduct]);
 
-    useEffect(() => {
-        const findResult = async () => {
-            try {
-                const response = await axios.get<ProductType[]>(apiUrl);
-                const results = response.data.filter(
-                    (item) => item.name.toLowerCase().includes(searchProduct.toLowerCase())
-                );
-                setSearchResult(results);
-            } catch (error) {
-                console.error("Error to search result:", error);
-            }
-        };
-    
-        if (searchProduct.trim() !== "") {
-            findResult();
-        } else {
-          setSearchResult([]);
-        }
-    }, [searchProduct]);
-
-    return (
-        <>
-            <div className={classes.search}>
-                <i className="material-symbols-outlined">search</i>
-                <input
-                    type="text"
-                    placeholder="Search headphone"
-                    onChange={handleInputChange}
-                    onClick={handleInputClick}
-                    className={classes.inputSearch}
+  return (
+    <>
+      <div className={classes.search}>
+        <i className="material-symbols-outlined">search</i>
+        <input
+          type="text"
+          placeholder="Search headphone"
+          onChange={handleInputChange}
+          onClick={handleInputClick}
+          className={classes.inputSearch}
+        />
+      </div>
+      <ul>
+        {searchProduct.trim() !== "" ? (
+          searchResult.length > 0 ? (
+            searchResult.map((product) => (
+              <li>
+                <ProductListCard
+                  key={product.id}
+                  productId={product.id}
+                  showReviews={true}
                 />
-            </div>
-            <div>
-                {searchProduct.trim() !== "" ? (searchResult.length > 0 ? (searchResult.map((product) => (
-                    <SearchCard key={product.id} productId={product.id} showReviews={true}/>
-                ))) : (
-                    <p>No product was found.</p>
-                )
-                ) : null}
-            </div>
-        </>
-    );
+              </li>  
+            ))
+          ) : (
+            <p className={classes.text}>No products found</p>
+          )
+        ) : null}
+      </ul>
+    </>
+  );
 };
 
 export default Search;
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
 import { useCartContext } from "../components/CartContext";
-import SearchCard from "../components/SearchCard";
+import ProductListCard from "../components/ProductListCard";
 import Button from "../components/Button";
 import classes from "./CartPage.module.css";
 
 const CartPage = () => {
-  const { cartProducts, addToCart, removeFromCart, removeProduct, removeAll } = useCartContext();
+  const { cartProducts, addToCart, removeFromCart, removeProduct, removeAll } =
+    useCartContext();
 
   const handleTrashClick = () => {
     removeAll();
@@ -47,34 +48,43 @@ const CartPage = () => {
 
   return (
     <div className={classes.container}>
-      <Header showText={true} text={"Shopping Cart"} showTrash={true} onClick={handleTrashClick}/>
+      <Header
+        showText={true}
+        text={"Shopping Cart"}
+        showTrash={true}
+        onClick={handleTrashClick}
+      />
 
       {cartProducts.length === 0 ? (
         <p>Empty shopping cart.</p>
       ) : (
-        <div>
+        <ul>
           {exclusiveId.map((productId) => (
             <div key={productId}>
-              <SearchCard 
-              productId={productId} 
-              showQuantity={true}
-              quantity={quantityProduct[productId]} 
-              addOne={addOneProduct}
-              removeOne={removeOneProduct}
-              removeProduct={removeProductFromCart}
-              />
+              <li>
+                <ProductListCard
+                  productId={productId}
+                  showQuantity={true}
+                  quantity={quantityProduct[productId]}
+                  addOne={addOneProduct}
+                  removeOne={removeOneProduct}
+                  removeProduct={removeProductFromCart}
+                />
+              </li>
             </div>
           ))}
-        </div>
+        </ul>
       )}
 
       <div className={classes.footer}>
-        <div className={classes.total}>      
-          <p className={classes.totalItems}>{`Total ${totalItems()} Item(s)`}</p>
+        <div className={classes.total}>
+          <p
+            className={classes.totalItems}
+          >{`Total ${totalItems()} Item(s)`}</p>
           <p className={classes.totalPrice}>{`USD ${totalPrice()}`}</p>
         </div>
-        <Button text={"Proceed to Checkout"} showArrow={true}/>
-      </div> 
+        <Button text={"Proceed to Checkout"} showArrow={true} />
+      </div>
     </div>
   );
 };
