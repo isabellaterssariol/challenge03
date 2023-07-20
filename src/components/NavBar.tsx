0860a236d1ff9cfe361bffd92f05869a4eaacfce
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-19 21:47:48 -0300

    Adding react-burger-menu.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,28 @@
+import { slide as Menu } from "react-burger-menu";
+import { Link } from "react-router-dom";
+import "./burger-menu-style/BurgerMenu.css";
+import classes from "./NavBar.module.css";
+
 interface NavBarProps {
   photo: string;
 }
 
 const NavBar: React.FC<NavBarProps> = ({ photo }) => {
     return (
-        <>  
-            <img src={photo} 
+        <div className={classes.container}>  
+            <div className={classes.burgerMenu}>
+                <Menu>
+                    <Link to="/" className={classes.link}>Home</Link>
+                    <Link to="/all-products" className={classes.link}>Products</Link>
+                    <Link to="/cart" className={classes.link}>Shopping Cart</Link>
+                </Menu>
+            </div>
+            <img 
+                src={photo} 
                 alt="User Avatar" 
+                className={classes.photoUser}
             />
-        </>
+        </div>
     );
 };
 
