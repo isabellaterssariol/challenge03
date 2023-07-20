0746c1247537780239fe260f35e9a8ed9f516527
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-07-20 12:41:12 -0300

    Adding Framer Motion.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import CategoryStore from "../components/CategoryStore";
 import Search from "../components/Search";
 import classes from "./HomePage.module.css";
 import NavBar from "../components/NavBar";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface HomePageProps {
   name: string;
@@ -12,18 +13,27 @@ interface HomePageProps {
 const HomePage: React.FC<HomePageProps> = ({ name, photo }) => {
     return (
         <div className={classes.container}>
-            <div>
-                <NavBar photo={photo}/>
-            </div> 
-            <div className={classes.welcomeText}>
-                <p className={classes.welcome}>{name ? `Hi, ${name}!` : "Hi, user!"}</p>
-                <p className={classes.text}>
-                    What are you looking for <br /> today?
-                </p>
-            </div>
-            <Search />
-            <CategoryStore />
-            <SomeProducts title={"Featured Products"}/>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    initial={{ x: "-100%" }}
+                    animate={{ x: 0 }}
+                    exit={{ x: "100%" }}
+                    transition={{ duration: 0.5 }}>
+                    <div>
+                        <NavBar photo={photo}/>
+                    </div> 
+                    <div className={classes.welcomeText}>
+                        <p className={classes.welcome}>{name ? `Hi, ${name}!` : "Hi, user!"}</p>
+                        <p className={classes.text}>
+                            What are you looking for <br /> today?
+                        </p>
+                    </div>
+                    <Search />
+                    <CategoryStore />
+                    <SomeProducts title={"Featured Products"}/>
+
+                </motion.div>
+            </AnimatePresence> 
         </div>
     );
 };
